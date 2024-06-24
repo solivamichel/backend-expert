f4e614c6d868568dbc0c58d9952222929de5104b
solivamichel <michelsolivadev@gmail.com>
2024-06-24 20:46:39 -0300

    first commit

---
+++ user-service-api/src/test/java/br/com/soliva/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soliva.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
