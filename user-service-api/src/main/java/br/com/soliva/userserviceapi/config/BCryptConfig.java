8da8eb0e396c94450ebfbd7b6bf5bb1d57ba97b3
solivamichel <michelsolivadev@gmail.com>
2024-06-27 11:59:25 -0300

    feat: add encryptor to password

---
+++ user-service-api/src/main/java/br/com/soliva/userserviceapi/config/BCryptConfig.java
@@ -0,0 +1,14 @@
+package br.com.soliva.userserviceapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BCryptConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
