d8f2eb714a1d6a6e5edf45009db64147e144b7f9
solivamichel <michelsolivadev@gmail.com>
2024-07-08 15:00:31 -0300

    test: add test to save user service

--- user-service-api/src/test/java/br/com/soliva/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/soliva/userserviceapi/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import br.com.soliva.userserviceapi.entity.User;
 import br.com.soliva.userserviceapi.mapper.UserMapper;
 import br.com.soliva.userserviceapi.repository.UserRepository;
 import models.exceptions.ResourceNotFoundException;
+import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
 import org.bson.assertions.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 import java.util.Optional;
 
+import static br.com.soliva.userserviceapi.creator.CreatorUtils.generateMock;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -41,7 +43,7 @@ class UserServiceTest {
     @DisplayName("When call findById with valid id then return UserResponse")
     void whenCallFindByIdValidIdThenReturnUserResponse() {
         when(repository.findById(anyString())).thenReturn(Optional.of(new User()));
-        when(mapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+        when(mapper.fromEntity(any(User.class))).thenReturn(generateMock(UserResponse.class));
 
         final var response = service.findById("1");
 
@@ -73,7 +75,7 @@ class UserServiceTest {
     @DisplayName("When call findAll then return list of UserResponse")
     void whenCallFindAllThenReturnListOfUserResponse() {
         when(repository.findAll()).thenReturn(List.of(new User(), new User()));
-        when(mapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+        when(mapper.fromEntity(any(User.class))).thenReturn(generateMock(UserResponse.class));
 
         final var response = service.findAll();
 
@@ -83,5 +85,24 @@ class UserServiceTest {
 
         verify(repository, times(1)).findAll();
         verify(mapper, times(2)).fromEntity(any(User.class));
+    }
+
+    @Test
+    @DisplayName("When call save then save user sucess")
+    void whenCallSaveThenSaveUserSucess() {
+        final var request = generateMock(CreateUserRequest.class);
+
+        when(mapper.fromRequest(any())).thenReturn(new User());
+        when(encoder.encode(anyString())).thenReturn("encoded");
+        when(repository.save(any(User.class))).thenReturn(new User());
+        when(repository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        service.save(request);
+
+        verify(mapper, times(1)).fromRequest(request);
+        verify(encoder, times(1)).encode(request.password());
+
+        verify(repository, times(1)).save(any(User.class));
+        verify(repository, times(1)).findByEmail(request.email());
     }
 }
