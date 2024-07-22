c9241c53bd805ae61bff3ce4a15409a5a388e3ea
solivamichel <michelsolivadev@gmail.com>
2024-07-22 14:02:34 -0300

    chore: starter auth-service-api

---
+++ auth-service-api/src/test/java/br/com/soliva/authserviceapi/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soliva.authserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
